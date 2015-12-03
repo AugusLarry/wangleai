308a429db2e8ae6bdaaa571f9661f519b420a809
lqm <resinchem@qq.com>
2015-12-03 14:20:08 +0800

    修复空模块不能跳转错误页面BUG

--- Web/Common/Conf/config.php
+++ Web/Common/Conf/config.php
@@ -4,4 +4,7 @@ return array(
 	'LOAD_EXT_CONFIG' => 'db,basic,verify,upload,page,comment',//自动加载配置文件
     'URL_MODEL'       =>  2,
 	'TMPL_CACHE_ON'   => false,
+	'MODULE_DENY_LIST' => ['Common', 'Runtime'],
+	'MODULE_ALLOW_LIST' => ['Index', 'Admin'],
+	'TMPL_EXCEPTION_FILE' => APP_PATH . 'Index/View/Common/exception.html'
 );
